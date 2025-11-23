f13de82bc52b7d56f5ffa64adae73ccf95016836
Arth Waghela <arthwaghela1996@gmail.com>
2025-11-23 23:03:03 +0530

    Fix Express 5 wildcard route for Angular

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ app.use(express.json());
 // Serve Angular frontend
 app.use(express.static(path.join(__dirname, "dist/admatazz-employee-project")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/admatazz-employee-project/index.html"));
+app.use((req, res, next) => {
+    if (!req.path.startsWith("/api")) {
+        res.sendFile(path.join(distPath, "index.html"));
+    } else {
+        next();
+    }
 });
 
 // API routes
